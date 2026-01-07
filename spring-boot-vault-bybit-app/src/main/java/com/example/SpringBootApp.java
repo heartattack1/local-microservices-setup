7979459bf492ecd72d054f0d9e7e9877bce02827
Rob <heartattack1@users.noreply.github.com>
2026-01-07 18:39:25 +0300

    Update SpringBootApp.java

--- spring-boot-vault-bybit-app/src/main/java/com/example/SpringBootApp.java
+++ spring-boot-vault-bybit-app/src/main/java/com/example/SpringBootApp.java
@@ -1,1 +1,11 @@
+package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVaultBybitApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVaultBybitApp.class, args);
+    }
+}
